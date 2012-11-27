d3c135f458fe3ac285f70ea3b42f93cbe0794692
Sergey Bogdanov <sergey.bogdanov@gmail.com>
2012-11-27 11:41:12 +0200

    added fix for forms authentication redirect

--- CustomAuthenticationMvc/Global.asax.cs
+++ CustomAuthenticationMvc/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using CustomAuthenticationMvc.App_Start;
+using CustomAuthenticationMvc.Helper;
 
 namespace CustomAuthenticationMvc
 {
---
+++ CustomAuthenticationMvc/Helper/SuppressFormsAuthenticationRedirectModule.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CustomAuthenticationMvc.Helper;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+[assembly: PreApplicationStartMethod(typeof(SuppressFormsAuthenticationRedirectModule), "Register")]
+
+namespace CustomAuthenticationMvc.Helper
+{
+    /// <summary>
+    /// Prevents Forms Authentication Login Page Redirect for AJAX requests:
+    /// http://haacked.com/archive/2011/10/04/prevent-forms-authentication-login-page-redirect-when-you-donrsquot-want.aspx
+    /// </summary>
+    public class SuppressFormsAuthenticationRedirectModule : IHttpModule
+    {
+        private static readonly object SuppressAuthenticationKey = new Object();
+
+        public static void SuppressAuthenticationRedirect(HttpContext context)
+        {
+            context.Items[SuppressAuthenticationKey] = true;
+        }
+
+        public static void SuppressAuthenticationRedirect(HttpContextBase context)
+        {
+            context.Items[SuppressAuthenticationKey] = true;
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.PostReleaseRequestState += OnPostReleaseRequestState;
+            context.EndRequest += OnEndRequest;
+        }
+
+        private void OnPostReleaseRequestState(object source, EventArgs args)
+        {
+            var context = (HttpApplication)source;
+            var response = context.Response;
+            var request = context.Request;
+
+            if (response.StatusCode == 401 && request.Headers["X-Requested-With"] ==
+              "XMLHttpRequest")
+            {
+                SuppressAuthenticationRedirect(context.Context);
+            }
+        }
+
+        private void OnEndRequest(object source, EventArgs args)
+        {
+            var context = (HttpApplication)source;
+            var response = context.Response;
+
+            if (context.Context.Items.Contains(SuppressAuthenticationKey))
+            {
+                response.TrySkipIisCustomErrors = true;
+                response.ClearContent();
+                response.StatusCode = 401;
+                response.RedirectLocation = null;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public static void Register()
+        {
+            DynamicModuleUtility.RegisterModule(
+              typeof(SuppressFormsAuthenticationRedirectModule));
+        }
+    }
+}
