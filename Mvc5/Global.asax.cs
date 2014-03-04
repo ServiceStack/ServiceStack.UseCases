073d0fcb67a24a4283ec6df3b0d2de600f8233fc
Demis Bellot <demis.bellot@gmail.com>
2014-03-04 03:25:53 -0500

    Add MVC5 and vs2013.sln to UseCases

---
+++ Mvc5/Global.asax.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+using ServiceStack;
+using ServiceStack.Text;
+using ServiceStack.Mvc;
+
+namespace Mvc5
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("MVC5", typeof(HelloServices).Assembly) { }
+
+        public override void Configure(Funq.Container container)
+        {
+            SetConfig(new HostConfig { HandlerFactoryPath = "api" });
+
+            //Set MVC to use the same Funq IOC as ServiceStack
+            ControllerBuilder.Current.SetControllerFactory(new FunqControllerFactory(container));
+        }
+    }
+
+    [ServiceStack.Route("/hello")]
+    public class Hello 
+    {
+        public string Name { get; set; }
+    }
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class HelloServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, {0}!".Fmt(request.Name) };
+        }
+    }
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            new AppHost().Init();
+        }
+    }
+}
