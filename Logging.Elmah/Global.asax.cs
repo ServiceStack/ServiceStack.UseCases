38ee5d8caafa767c394e73c24d42f1d0afd66bc0
Demis Bellot <demis.bellot@gmail.com>
2014-08-07 08:04:16 -0400

    Add Logging.Elmah

---
+++ Logging.Elmah/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Funq;
+using ServiceStack;
+using ServiceStack.Logging;
+using ServiceStack.Logging.Elmah;
+
+namespace Logging.Elmah
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("Logging.Elmah", typeof (MyServices).Assembly) {}
+
+        public override void Configure(Container container)
+        {
+        }
+    }
+
+    [Route("/logerror/{Text}")]
+    public class LogError
+    {
+        public string Text { get; set; }
+    }
+
+    public class MyServices : Service
+    {
+        public object Any(LogError request)
+        {
+            throw new Exception(request.Text);
+        }
+    }
+
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //View Elmah Error Log at: /elmah.axd
+            var debugMessagesLog = new ConsoleLogFactory();
+            LogManager.LogFactory = new ElmahLogFactory(debugMessagesLog, this);
+            new AppHost().Init();
+        }
+    }
+}
