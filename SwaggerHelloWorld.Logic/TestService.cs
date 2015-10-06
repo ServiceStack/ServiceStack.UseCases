f9e58cea0e52218847997a17dc7a39b5180176bd
Demis Bellot <demis.bellot@gmail.com>
2015-10-06 16:09:06 -0700

    Add new SwaggerHelloWorld.Logic test project

---
+++ SwaggerHelloWorld.Logic/TestService.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack;
+
+namespace SwaggerHelloWorld.Logic
+{
+    [Route("/test")]
+    public class Test : IReturn<Test>
+    {
+        public int Id { get; set; }
+    }
+
+    public class TestService : Service
+    {
+        public object Any(Test request)
+        {
+            return request;
+        }
+    }
+}
