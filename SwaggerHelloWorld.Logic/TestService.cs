63db511b87a019a554c841fddb60f8b85ad8ed78
Demis Bellot <demis.bellot@gmail.com>
2016-01-01 00:07:41 -0800

    Add TestComplex test service

--- SwaggerHelloWorld.Logic/TestService.cs
+++ SwaggerHelloWorld.Logic/TestService.cs
@@ -6,11 +6,23 @@ namespace SwaggerHelloWorld.Logic
     public class Test : IReturn<Test>
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Route("/testcomplex")]
+    public class TestComplex
+    {
+        public Test Test { get; set; }
     }
 
     public class TestService : Service
     {
         public object Any(Test request)
+        {
+            return request;
+        }
+
+        public object Any(TestComplex request)
         {
             return request;
         }
